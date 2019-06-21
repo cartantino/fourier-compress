352cdda2d007214a0c3b379c491da16b4e11e580
Pietro Colombo <pietrocolombo@me.com>
2019-06-21 15:42:34 +0200

    gestita eccezione di premere prima esegui che il caricamento dell'immagine

--- interface.py
+++ interface.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QInputDialog, QLineEdit, QFileDialog, QPushButton, QLabel
+from PyQt5.QtWidgets import QApplication, QWidget, QInputDialog, QLineEdit, QFileDialog, QPushButton, QLabel, QMessageBox
 from PyQt5.QtGui import QIcon
 from PyQt5.QtCore import pyqtSlot, Qt
 from PyQt5.QtGui import QIcon, QPixmap
@@ -20,7 +20,7 @@ class App(QWidget):
         self.title = 'Metodi del calcolo Scientifico'
         self.left = 10
         self.top = 10
-        self.width = 1280
+        self.width = 1080
         self.height = 960
         self.file_path = None
         self.textboxValue_f = 0
@@ -35,23 +35,29 @@ class App(QWidget):
         
         self.button_carica = QPushButton('Carica immagine', self)
         self.button_carica.setToolTip('Carica immagine')
-        self.button_carica.move(500,50)
+        self.button_carica.move(176,46)
         self.button_carica.clicked.connect(self.on_click_carica)
 
         # Create textox for f
-        self.textbox_f = QLineEdit(self, placeholderText="Insert F")
-        self.textbox_f.move(200, 100)
-        self.textbox_f.resize(100,40)
+        self.text_f = QLabel(self)
+        self.text_f.setText('Insert F:')
+        self.textbox_f = QLineEdit(self, placeholderText="")
+        self.textbox_f.move(180, 100)
+        self.textbox_f.resize(150,40)
+        self.text_f.move(120, 110)
 
         # Create textbox for d
-        self.textbox_d = QLineEdit(self, placeholderText="Insert D")
-        self.textbox_d.move(200, 150)
-        self.textbox_d.resize(100,40)
+        self.text_d = QLabel(self)
+        self.text_d.setText('Insert D = 0 to (2F − 2)')
+        self.textbox_d = QLineEdit(self, placeholderText="")
+        self.textbox_d.move(180, 150)
+        self.textbox_d.resize(150,40)
+        self.text_d.move(20, 160)
 
         # connect button to function on_click_d_f
         # Create a button in the window
         self.button_d_f = QPushButton('Esegui', self)
-        self.button_d_f.move(500,200)
+        self.button_d_f.move(175,210)
         self.button_d_f.clicked.connect(self.on_click_d_f)
 
         #self.saveFileDialog()
@@ -72,6 +78,11 @@ class App(QWidget):
         else:
             self.first_original = False
         
+        if(self.first_compress != True):
+            self.immage_compress.hide()
+        else:
+            self.first_compress = False
+        
         self.immage_original = QLabel(self)
         pixmap = QPixmap(fileName)
         pixmap_resized = pixmap.scaled(400, 500, Qt.KeepAspectRatio)
@@ -83,27 +94,27 @@ class App(QWidget):
     
     @pyqtSlot()
     def on_click_d_f(self):
-        self.textboxValue_f = int(self.textbox_f.text())
-        self.textboxValue_d = int(self.textbox_d.text())
-        if(self.first_compress != True):
-            self.immage_compress.hide()
-        else:
-            self.first_compress = False
-        
-        image = misc.imread(self.file_path, flatten = 0)
-        if (image.ndim >= 3):
-            image = image[:,:,0]
-        immage_compress = self.dct_compression(image, self.textboxValue_f, self.textboxValue_d)
-        path_save = self.file_path + "_compress.bmp"
-        misc.imsave(path_save, immage_compress)
+        if(not(self.first_original)):
+           self.textboxValue_f = int(self.textbox_f.text())
+           self.textboxValue_d = int(self.textbox_d.text())
 
-        self.immage_compress = QLabel(self)
-        pixmap = QPixmap(path_save)
-        pixmap_resized = pixmap.scaled(400, 500, Qt.KeepAspectRatio)
-        self.immage_compress.setPixmap(pixmap_resized)
-        #self.resize(pixmap.width(),pixmap.height())
-        self.immage_compress.move(500,300)
-        self.immage_compress.show()
+           image = misc.imread(self.file_path, flatten = 0)
+           if (image.ndim >= 3):
+               image = image[:,:,0]
+           immage_compress = self.dct_compression(image, self.textboxValue_f, self.textboxValue_d)
+           path_save = self.file_path + "_compress.bmp"
+           misc.imsave(path_save, immage_compress)
+
+           self.immage_compress = QLabel(self)
+           pixmap = QPixmap(path_save)
+           pixmap_resized = pixmap.scaled(400, 500, Qt.KeepAspectRatio)
+           self.immage_compress.setPixmap(pixmap_resized)
+           #self.resize(pixmap.width(),pixmap.height())
+           self.immage_compress.move(500,300)
+           self.immage_compress.show()
+        else:
+            QMessageBox.question(self, 'ERRORE', 'Caricare prima l\'immagine', QMessageBox.Ok, QMessageBox.Ok)
+        
 
 
     def dct_compression(self, image, F, d):
